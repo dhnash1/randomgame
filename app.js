4c7e2e25de92114d52541bf9350c1f3ae1c2baeb
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2016-12-09 16:30:48 -0600

    Updating deforis' repo

--- app.js
+++ app.js
@@ -1,39 +1,50 @@
-// required garbage
 var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-var urlEncodedParser = bodyParser.urlencoded({extended:false});
+var urlEncodedParser = bodyParser.urlencoded({extended:true});
 var maxNum;
+var gameNumber;
+var playersArray;
 
+function rand(maxNum){
+  return parseInt(Math.random() * maxNum) + 1;
+} // end rand
 
-var rand = function(maxNum){
-  var randomNumber = parseInt(Math.random() * maxNum) + 1;
-  console.log(randomNumber);
-
-};
 app.use(express.static('public'));
-//port listening
+
 app.listen(1776, function(){
   console.log('Listening!');
-});
-//Start button click
-app.post('/getMax', urlEncodedParser, function(req, res){
-  console.log('/getMax url hit');
+}); //app listen end
+
+app.post('/postMax', urlEncodedParser, function(req, res){
+  console.log('/postMax url hit');
   console.log('req.body:',req.body);
   res.send(req.body);
   maxNum = req.body.num;
-  var gameNumber = rand(maxNum);
+  gameNumber = rand(maxNum);
   console.log(maxNum);
   console.log(gameNumber);
-});
+}); //postMax end
 
-
-//submit button click
 app.post('/postInputs', urlEncodedParser, function(req, res){
   console.log('/postInputs url hit');
-  console.log('req.body:',req.body);
-  var playerInputs = req.body;
-  console.log('P inputs: ' +  playerInputs.one);
-  res.send(req.body);
-});
+//  console.log('req.body:',req.body);
+  playersArray = req.body.array;
+  console.log("playersArray on server:", playersArray, "gameNumber:", gameNumber, playersArray[0].guess);
+  compare();
+  console.log("playersArray after compare:", playersArray);
+  res.send({array: playersArray});
+}); //postInputs end
+
+function compare(){
+  for (var i = 0; i < playersArray.length; i++) {
+    if (playersArray[i].guess == gameNumber) {
+      playersArray[i].highLow = 'Winner';
+    } else if (playersArray[i].guess < gameNumber) {
+      playersArray[i].highLow = 'Low';
+    } else if (playersArray[i].guess > gameNumber) {
+      playersArray[i].highLow = 'High';
+    } // end else/if
+  } // end for
+} // end compare
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,53 +1,108 @@
-console.log("SOurced");
+var playersArray = [];
+var counter;
+var logs = false;
+
+function Player(playerName, guess, highLow, howClose){
+  this.playerName = playerName;
+  this.guess = guess;
+  this.highLow = highLow;
+  playersArray.push(this);
+} // end Player
 
 var postMaxNum = function(num) {
   $.ajax({
     type: "POST",
     data: {num: num},
-    url: '/getMax',
+    url: '/postMax',
     success: function(response) {
-      console.log('postMaxNum ajax success');
+      if (logs) console.log('postMaxNum ajax success');
     },
     error: function(){
-      console.log('get max ajax error');
+      if (logs) console.log('get max ajax error');
     }
-  });
+  }); // end ajax
 }; // end postMaxNum
 
-var postInputs = function(thing) {
+var postInputs = function(playersArray) {
   $.ajax({
     type: "POST",
-    data: thing,
+    data: {array: playersArray},
     url: '/postInputs',
     success: function(response) {
-      console.log('postInputs ajax success');
+      if (logs) console.log('postInputs ajax success; response:', response);
+      findWinner(response.array);
+      // display(response.array);
     },
     error: function(){
-      console.log('get max ajax error');
+      if (logs) console.log('get max ajax error');
     }
-  });
+  }); // end ajax
 }; // end postMaxNum
-//send maximum number input
+function findWinner(array) {
+  var foundWinner = false;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].highLow == "Winner") {
+      alert(array[i].playerName + ' wins!');
+      foundWinner = true;
+    } // end if
+  } // end for
+  if (foundWinner === false) {
+    display(array);
+  } else {
+    //display winner
+    $('#abandon').text('Restart Game');
+  } // end else
+} // end findWinner
+
+function display(replacementArray){
+  if (logs) console.log("Player array after server handling: ", replacementArray);
+  for (var i = 0; i < replacementArray.length; i++) {
+    $('#pastGuess').append("<p class='player-name'>" + replacementArray[i].playerName + "</p>");
+    $('#pastGuess').append('<p>Last Guess: '+replacementArray[i].guess+'</p>');
+    $('#pastGuess').append("<p>"+ replacementArray[i].highLow +  "</p>");
+  } // end for
+} // end display
+
 $(document).ready(function(){
-  console.log('JQ');
+  $('#playMode').hide();
+  $('#pastGuess').hide();
   //event listeners
   $('#startButton').on('click', function(){
-    console.log('start clicked');
+    $('#abandon').text('Quit!');
+    counter = 0;
+    $('#inputMode').hide();
+    $('#playMode').show();
     var maxNum = $('#maxNumIn').val();
-    console.log(maxNum);
+    $('#maxNumber').html('<h2>Maximum Number: '+maxNum+'</h2>');
+    if (logs) console.log(maxNum);
     postMaxNum(maxNum);
+    var playerOne = new Player("Player 1");
+    var playerTwo = new Player("Player 2");
+    var playerThree = new Player("Player 3");
+    var playerFour = new Player("Player 4");
   }); // end #startButtonnp
 
-  //send inputs
   $('#submit').on('click', function(){
-    console.log('submit clicked');
-    var inputs = {
-      one:$('#playerOne').val(),
-      two:$('#playerTwo').val(),
-      three:$('#playerThree').val(),
-      four:$('#playerFour').val()
-    };
-    postInputs(inputs);
+    $('#pastGuess').html('');
+    $('#pastGuess').show();
+    if (logs) console.log("Player array before server handling: ", playersArray);
+    counter++;
+    $('#count').html('<p> Attempts: ' + counter + '</p>');
+    playersArray[0].guess = $('#playerOne').val();
+    playersArray[1].guess = $('#playerTwo').val();
+    playersArray[2].guess = $('#playerThree').val();
+    playersArray[3].guess = $('#playerFour').val();
+    postInputs(playersArray);
+    //clear input values
+    $('input').val('');
   }); // end #startButtonnp
 
+  $('#abandon').on('click', function() {
+    if (logs) console.log('quit clicked');
+    counter = 0;
+    $('#count').html('<p>Attempts: '+ counter + '</p>');
+    $('#pastGuess').html('');
+    $('#playMode').hide();
+    $('#inputMode').show();
+  }); // end #abandon
 });
